b0d8a0b888d1bc154ef2205c9119c4ac26f40c7b
chargeAhead <eric.aganze@strathmore.edu>
2020-04-28 18:26:37 +0300

    initial commit

---
+++ IAPLABS/Crud.php
@@ -0,0 +1,19 @@
+<?php
+  /*
+   functions to be imlemented by classes that implement this interface
+   */
+  interface Crud
+  {
+    public function save($con);
+    public static function readAll($con);
+    public function readUnique();
+    public function search();
+    public function update();
+    public function removeOne();
+    public function removeAll();
+
+    public function validateForm();
+    public function createFormErrorSessions();
+  }
+
+?>
---
+++ IAPLABS/DBConnector.php
@@ -0,0 +1,27 @@
+<?php
+  define('DB_SERVER', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', 'ics3106');
+
+  /**
+   *
+   */
+  class DBConnector
+  {
+    public $conn;
+
+    function __construct()
+    {
+      $this->conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS) or die("Error: ".mysqli_error());
+      // var_dump($this->conn);
+      // die();
+      mysqli_select_db($this->conn, DB_NAME);
+    }
+
+    public function closeDatabase(){
+      mysqli_close($this->conn);
+    }
+  }
+
+?>
